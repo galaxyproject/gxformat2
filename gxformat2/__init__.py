2257f7162d427c4be25edbb876bb7b150fb5ca7c
John Chilton <jmchilton@gmail.com>
2019-05-23 12:55:55 -0400

    Starting work on 0.9.0

--- gxformat2/__init__.py
+++ gxformat2/__init__.py
@@ -1,6 +1,6 @@
 """The public interface or entry point for the Format 2 workflow code."""
 
-__version__ = '0.8.3'
+__version__ = '0.9.0.dev0'
 
 PROJECT_NAME = "gxformat2"
 PROJECT_OWNER = PROJECT_USERAME = "jmchilton"
