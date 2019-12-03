f1070ff37141520cc85f0ba07edd7234319bd812
John Chilton <jmchilton@gmail.com>
2019-12-03 09:40:28 -0500

    Github publish docs actions (3/n)
    Option to skip java.

--- build_schema.sh
+++ build_schema.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 PROJECT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
+SKIP_JAVA=${SKIP_JAVA:-0}
 
 # Requires schema-salad-doc that recognizes --brandstyle and --brandinverse
 for schema in "v19.09";
@@ -22,14 +22,16 @@ do
         --only "https://galaxyproject.org/gxformat2/${schema}#GalaxyWorkflow" \
         workflow.yml > "$out"
 
-    java_package="${PROJECT_DIRECTORY}/java"
-    schema-salad-tool \
-        --codegen java \
-        --codegen-target "$java_package" \
-        --codegen-examples examples \
-        workflow.yml
-    cd "$java_package"
-    mvn test
-    mvn javadoc:javadoc
-    cd "${PROJECT_DIRECTORY}"
+    if [ $SKIP_JAVA -eq 0 ]; then
+        java_package="${PROJECT_DIRECTORY}/java"
+        schema-salad-tool \
+            --codegen java \
+            --codegen-target "$java_package" \
+            --codegen-examples examples \
+            workflow.yml
+        cd "$java_package"
+        mvn test
+        mvn javadoc:javadoc
+        cd "${PROJECT_DIRECTORY}"
+    fi
 done
