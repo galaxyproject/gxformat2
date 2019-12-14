d1c742a6732af04cc32e86644cd8cc90df55931b
John Chilton <jmchilton@gmail.com>
2019-12-14 12:01:10 -0500

    Implement cytoscape export for Java.

--- java/src/test/java/org/galaxyproject/gxformat2/CytoscapeExamplesTest.java
+++ java/src/test/java/org/galaxyproject/gxformat2/CytoscapeExamplesTest.java
@@ -6,11 +6,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.Assert;
+import org.junit.Test;
 
 public class CytoscapeExamplesTest {
 
-  // WIP!!!
-  // @Test
+  @Test
   public void testExamples() throws Exception {
     for (final File file : TestUtils.getExamples("cytoscape")) {
       final String path = file.getAbsolutePath();
@@ -33,7 +33,39 @@ public class CytoscapeExamplesTest {
       List<Map<String, Object>> elements = Cytoscape.getElements(path);
       Map<String, Map<String, Object>> elementsById = getElementsById(elements);
       Map<String, Map<String, Object>> expectedElementsById = getElementsById(expectedElements);
-      Assert.assertEquals(elementsById.size(), expectedElementsById.size());
+      Assert.assertEquals(expectedElementsById.size(), elementsById.size());
+      for (final Map.Entry<String, Map<String, Object>> elementEntry : elementsById.entrySet()) {
+        final String elementKey = elementEntry.getKey();
+        Assert.assertTrue(expectedElementsById.containsKey(elementKey));
+        assertElementsEqual(expectedElementsById.get(elementKey), elementEntry.getValue());
+      }
+    }
+  }
+
+  private static void assertElementsEqual(
+      final Map<String, Object> expectedElements, final Map<String, Object> actualObjects) {
+    for (Map.Entry entry : expectedElements.entrySet()) {
+      Object key = entry.getKey();
+      Assert.assertTrue("Actual elements lacks key " + key, actualObjects.containsKey(key));
+      final Object expectedValue = entry.getValue();
+      final Object actualValue = actualObjects.get(key);
+      if (actualValue instanceof Map) {
+        Assert.assertTrue(expectedValue instanceof Map);
+        assertElementsEqual((Map<String, Object>) actualValue, (Map<String, Object>) expectedValue);
+      } else if (actualValue instanceof Long || actualValue instanceof Integer) {
+        Assert.assertTrue(expectedValue instanceof Long || expectedValue instanceof Integer);
+        Assert.assertEquals(asLong(expectedValue), asLong(actualValue));
+      } else {
+        Assert.assertEquals("Elements not equal for key " + key, expectedValue, actualValue);
+      }
+    }
+  }
+
+  static long asLong(Object object) {
+    if (object instanceof Integer) {
+      return ((Integer) object).longValue();
+    } else {
+      return (Long) object;
     }
   }
 
@@ -41,7 +73,7 @@ public class CytoscapeExamplesTest {
       List<Map<String, Object>> elements) {
     Map<String, Map<String, Object>> elementsById = new HashMap();
     for (Map<String, Object> element : elements) {
-      elementsById.put((String) element.get("id"), element);
+      elementsById.put((String) ((Map<String, Object>) element.get("data")).get("id"), element);
     }
     return elementsById;
   }
---
+++ tests/test_cytoscape.py
@@ -0,0 +1,49 @@
+import json
+import os
+import shutil
+import tempfile
+
+from gxformat2.cytoscape import main
+from ._helpers import TEST_INTEROP_EXAMPLES, TEST_PATH
+from .test_lint import WITH_REPORT
+
+EXAMPLE_PATH = os.path.join(TEST_PATH, "unicycler-extra-annotations.ga")
+TEST_CYTOSCAPE_EXAMPLES = os.path.join(TEST_INTEROP_EXAMPLES, "cytoscape")
+
+
+def test_main_output_json():
+    out_file = tempfile.NamedTemporaryFile(prefix="cytoscape_elements", suffix=".json")
+    main([EXAMPLE_PATH, out_file.name])
+    with open(out_file.name, "r") as f:
+        elements = json.load(f)
+    assert isinstance(elements, list)
+    assert "</body>" not in open(out_file.name, "r").read()
+
+
+def test_main_output_html():
+    out_file = tempfile.NamedTemporaryFile(prefix="cytoscape_elements", suffix=".html")
+    main([EXAMPLE_PATH, out_file.name])
+    assert "</body>" in open(out_file.name, "r").read()
+
+
+def test_interop_generation():
+    # not much of a test case but it will generate a directory of interoperability examples to
+    # test Java against.
+    write_cytoscape_elements(EXAMPLE_PATH)
+    write_cytoscape_elements_for_string(WITH_REPORT)
+
+
+def write_cytoscape_elements_for_string(workflow_content):
+    f = tempfile.NamedTemporaryFile(mode="w", suffix=".gxwf.yml")
+    f.write(workflow_content)
+    f.flush()
+    write_cytoscape_elements(f.name)
+
+
+def write_cytoscape_elements(workflow_path):
+    if not os.path.exists(TEST_CYTOSCAPE_EXAMPLES):
+        os.makedirs(TEST_CYTOSCAPE_EXAMPLES)
+    base_name, ext = os.path.splitext(os.path.basename(workflow_path))
+    shutil.copyfile(workflow_path, os.path.join(TEST_CYTOSCAPE_EXAMPLES, base_name + ext))
+    elements_path = os.path.join(TEST_CYTOSCAPE_EXAMPLES, base_name + ".cytoscape.json")
+    main([workflow_path, elements_path])
