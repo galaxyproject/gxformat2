b4ea2a91738e0fe4e1530af28e5ed65f6c25a1a5
John Chilton <jmchilton@gmail.com>
2020-08-03 19:31:06 -0400

    Starting work on 0.14.0

--- gxformat2/__init__.py
+++ gxformat2/__init__.py
@@ -1,6 +1,6 @@
 """The public interface or entry point for the Format 2 workflow code."""
 
-__version__ = '0.13.1'
+__version__ = '0.14.0.dev0'
 
 PROJECT_NAME = "gxformat2"
 PROJECT_OWNER = PROJECT_USERAME = "galaxyproject"
