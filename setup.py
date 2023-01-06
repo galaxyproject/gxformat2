d79f20119dcf943b5423d088b13842c9027b4fc3
mvdbeek <m.vandenbeek@gmail.com>
2023-01-06 09:39:48 +0200

    Bump supported python version range from 3.7 to 3.10

--- setup.py
+++ setup.py
@@ -93,10 +93,11 @@ setup(
         'Topic :: Software Development :: Testing',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     test_suite=TEST_DIR,
     tests_require=test_requirements
