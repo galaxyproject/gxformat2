1f0cb7a9e4351c1823640023f524d39f3dbb673b
mvdbeek <m.vandenbeek@gmail.com>
2024-08-23 16:08:40 +0200

    Rebuild java and typescrict generated code

--- java/src/main/java/org/galaxyproject/gxformat2/GalaxyWorkflowLinter.java
+++
@@ -1,7 +0,0 @@
-package org.galaxyproject.gxformat2;
-
-import java.util.Map;
-
-public interface GalaxyWorkflowLinter {
-  public void lint(final LintContext lintContext, final Map<String, Object> workflow);
-}
