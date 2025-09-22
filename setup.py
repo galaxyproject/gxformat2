093579c290abbfb83dd8b6e4e18bd323cb3718b4
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2025-09-22 13:45:51 +0100

    Add support for Python 3.14

--- setup.py
+++ setup.py
@@ -98,6 +98,7 @@ setup(
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
     ],
     test_suite=TEST_DIR,
     tests_require=test_requirements
