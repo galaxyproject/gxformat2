63f28e2ef64ad7ade07326114a505907b807a348
John Chilton <jmchilton@gmail.com>
2026-03-23 15:32:11 -0400

    Missing import.

--- tests/test_to_format2.py
+++ tests/test_to_format2.py
@@ -6,6 +6,7 @@ from yaml import safe_load
 
 from gxformat2.converter import python_to_workflow
 from gxformat2.export import from_galaxy_native, main
+from gxformat2.yaml import ordered_load
 from ._helpers import example_path, MockGalaxyInterface, TEST_PATH, to_example_path
 
 
